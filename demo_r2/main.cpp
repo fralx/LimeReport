8061d162dc255f761a2b3a2fc817950be3193b30
Arin Alex <arin_a@bk.ru>
2017-10-05 02:47:45 +0300

    Demo2 fixed

--- demo_r2/main.cpp
+++ demo_r2/main.cpp
@@ -4,18 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QFile f(":qdarkstyle/style.qss");
-    if (!f.exists())
-    {
-        printf("Unable to set stylesheet, file not found\n");
-    }
-    else
-    {
-        f.open(QFile::ReadOnly | QFile::Text);
-        QTextStream ts(&f);
-        a.setStyleSheet(ts.readAll());
-        //a.setStyleSheet("");
-    }
+
     MainWindow w;
     w.show();
 
