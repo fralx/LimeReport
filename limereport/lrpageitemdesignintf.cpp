ca6241f61eb8ea3f777749f493a4883bd05d53d1
Arin Alexander <arin_a@bk.ru>
2020-04-23 21:03:22 +0300

    Band moving in design mode has been fixed

--- limereport/lrpageitemdesignintf.cpp
+++ limereport/lrpageitemdesignintf.cpp
@@ -876,8 +876,8 @@ void PageItemDesignIntf::moveBandFromTo(int from, int to)
 
 void PageItemDesignIntf::bandPositionChanged(QObject* object, QPointF newPos, QPointF oldPos)
 {
-    if (itemMode() == DesignMode){
-        BandDesignIntf* band = dynamic_cast<BandDesignIntf*>(object);
+    BandDesignIntf* band = dynamic_cast<BandDesignIntf*>(object);
+    if (band && !band->isChangingPos() && (itemMode() == DesignMode)){
         int curIndex = band->bandIndex();
         BandDesignIntf* bandToSwap = 0;
         foreach(BandDesignIntf* curBand, bands()){
@@ -906,7 +906,8 @@ void PageItemDesignIntf::bandPositionChanged(QObject* object, QPointF newPos, QP
                 page()->saveCommand(BandMoveFromToCommand::create(page(), band->bandIndex(), bandToSwap->bandIndex()), true);
         }
     }
-    relocateBands();
+    if (band && !band->isChangingPos())
+        relocateBands();
 }
 
 void PageItemDesignIntf::bandGeometryChanged(QObject* object, QRectF newGeometry, QRectF oldGeometry)
